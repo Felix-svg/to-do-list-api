c546a2c5754a80698018867d01db719410fe5cd1
Felix Omondi <felixomosh7@gmail.com>
2024-08-18 12:05:29 +0300

    Add user model routes

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from config import app, api
-from routes import Todos, TodoByID
+from routes import Todos, TodoByID, Users, UserByID
 
 # routes
-api.add_resource(Todos, "/todos")
-api.add_resource(TodoByID, "/todos/<int:id>")
+api.add_resource(Users, "/api/users/")
+api.add_resource(UserByID, "/api/users/<int:id>/")
+api.add_resource(Todos, "/api/todos/")
+api.add_resource(TodoByID, "/api/todos/<int:id>/")
 
 
 if __name__ == "__main__":
--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import request, make_response
 from flask_restful import Resource
-from models import Todo
+from models import Todo, User
 from utils import (
     server_error,
     no_input,
@@ -13,10 +13,99 @@ from utils import (
 from config import db
 
 
+class Users(Resource):
+    def get(self):
+        try:
+            users = [user.to_dict(only=("email", "name")) for user in User.query.all()]
+            return make_response(users, 200)
+        except Exception as e:
+            return server_error(e)
+
+    def post(self):
+        try:
+            data = request.get_json()
+            if not data:
+                return no_input()
+
+            name = data.get("name")
+            email = data.get("email")
+            password = data.get("password")
+
+            if not name or not email or not password:
+                return missing_required_fields()
+
+            if User.query.filter(User.email == email).first():
+                return make_response({"message": "Email already exists"}, 400)
+
+            new_user = User(name=name, email=email)
+            new_user.set_password(password)
+            db.session.add(new_user)
+            db.session.commit()
+            return created("User")
+        except Exception as e:
+            db.session.rollback()
+            return server_error(e)
+
+
+class UserByID(Resource):
+    def get(self, id):
+        try:
+            # user = User.query.filter(User.id == id).first_or_404(description="User not found")
+            user = User.query.filter(User.id == id).first()
+            if not user:
+                return not_found("User")
+
+            user_dict = user.to_dict(only=("name", "email", "id"))
+            return make_response(user_dict, 200)
+        except Exception as e:
+            return server_error(e)
+
+    def patch(self, id):
+        try:
+            user = User.query.filter(User.id == id).first()
+            if not user:
+                return not_found("User")
+
+            data = request.get_json()
+            if not data:
+                return no_input()
+
+            name = data.get("name")
+            email = data.get("email")
+            password = data.get("password")
+
+            if name is not None:
+                user.name = name
+            if email is not None:
+                user.email = email
+            if password is not None:
+                user.set_password(password)
+
+            db.session.commit()
+            return updated("User")
+        except Exception as e:
+            db.session.rollback()
+            return server_error(e)
+
+    def delete(self, id):
+        try:
+
+            user = User.query.filter(User.id == id).first()
+            if not user:
+                return not_found("User")
+
+            db.session.delete(user)
+            db.session.commit()
+            return deleted("User")
+        except Exception as e:
+            db.session.rollback()
+            return server_error(e)
+
+
 class Todos(Resource):
     def get(self):
         try:
-            todos = [todo.to_dict() for todo in Todo.query.all()]
+            todos = [todo.to_dict(rules=["-user"]) for todo in Todo.query.all()]
             return make_response(todos, 200)
         except Exception as e:
             return server_error(e)
@@ -45,7 +134,7 @@ class TodoByID(Resource):
             todo = Todo.query.filter(Todo.id == id).first()
             if not todo:
                 return not_found("Task")
-            todo_dict = todo.to_dict()
+            todo_dict = todo.to_dict(rules=["-user"])
             return make_response(todo_dict, 200)
         except Exception as e:
             return server_error(e)
@@ -71,7 +160,7 @@ class TodoByID(Resource):
             db.session.commit()
             return updated("Task")
         except Exception as e:
-            db.session.rollback()
+            db.session.rollback(e)
             return server_error()
 
     def delete(self, id):
